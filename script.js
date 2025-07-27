3d7db609d4fe05ec72a8be13ad30028519afb445
Abdul Al Mamun <mail4mystudies@gmail.com>
2025-07-27 22:25:37 +0600

    code is super-smart and animations added

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
 const rock = document.querySelector(".rock");
-const textRock = document.querySelector(".text-rock");
 const paper = document.querySelector(".paper");
-const textPaper = document.querySelector(".text-paper");
 const scissor = document.querySelector(".scissor");
-const textScissor = document.querySelector(".text-scissor");
 const decision = document.querySelector(".absolute");
 
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const array = ["Rock", "Paper", "Scissor"];
 
-function executionRock() {
-  left.src = `./images/left/0.png`;
+function execution() {
+  left.src = `./images/left/rock.png`;
+  right.src = `./images/right/rock.png`;
 
+  left.classList.add("animateL");
+  right.classList.add("animateR");
+
+  setTimeout(() => {
+    left.classList.remove("animateL");
+    right.classList.remove("animateR");
+  }, 3000);
+
+  let name = this.textContent;
+  name = name.trim();
   let guess = Math.floor(Math.random() * 3);
-  right.src = `./images/right/${guess}.png`;
 
-  if (array[guess] == "Rock") {
-    decision.textContent = "Draw";
-  } else if (array[guess] == "Paper") {
-    decision.textContent = "You lose";
-  } else if (array[guess] == "Scissor") {
-    decision.textContent = "You Win!";
-  }
+  setTimeout(() => {
+    left.src = `./images/left/${name}.png`;
+    right.src = `./images/right/${array[guess]}.png`;
+
+    if (name == array[0]) {
+      if (guess == 0) {
+        decision.textContent = "Draw";
+      } else if (guess == 1) {
+        decision.textContent = "You lost";
+      } else {
+        decision.textContent = "You won";
+      }
+    }
+    if (name == array[1]) {
+      if (guess == 1) {
+        decision.textContent = "Draw";
+      } else if (guess == 2) {
+        decision.textContent = "You lost";
+      } else {
+        decision.textContent = "You won";
+      }
+    }
+    if (name == array[2]) {
+      if (guess == 2) {
+        decision.textContent = "Draw";
+      } else if (guess == 0) {
+        decision.textContent = "You lost";
+      } else {
+        decision.textContent = "You won";
+      }
+    }
+  }, 3000);
 }
-function executionPaper() {
-  left.src = `./images/left/1.png`;
 
-  let guess = Math.floor(Math.random() * 3);
-  right.src = `./images/right/${guess}.png`;
-
-  if (array[guess] == "Rock") {
-    decision.textContent = "You Win!";
-  } else if (array[guess] == "Paper") {
-    decision.textContent = "Draw";
-  } else if (array[guess] == "Scissor") {
-    decision.textContent = "You lose";
-  }
-}
-function executionScissor() {
-  left.src = `./images/left/2.png`;
-
-  let guess = Math.floor(Math.random() * 3);
-  right.src = `./images/right/${guess}.png`;
-
-  if (array[guess] == "Rock") {
-    decision.textContent = "You lose";
-  } else if (array[guess] == "Paper") {
-    decision.textContent = "You Win!";
-  } else if (array[guess] == "Scissor") {
-    decision.textContent = "Draw";
-  }
-}
-rock.addEventListener("click", executionRock);
-paper.addEventListener("click", executionPaper);
-scissor.addEventListener("click", executionScissor);
+document.querySelector(".rock").addEventListener("click", execution);
+document.querySelector(".paper").addEventListener("click", execution);
+document.querySelector(".scissor").addEventListener("click", execution);
