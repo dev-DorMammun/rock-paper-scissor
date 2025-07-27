38eef88c8e49fd55f23ce6f152b2f7bed3042de9
Abdul Al Mamun <mail4mystudies@gmail.com>
2025-07-28 00:46:50 +0600

    attempt 7

--- script.js
+++ script.js
@@ -24,9 +24,6 @@ function execution() {
   }, 3000);
 
   setTimeout(() => {
-    left.src = `./images/left/${name}.png`;
-    right.src = `./images/right/${array[guess]}.png`;
-
     if (name == array[0]) {
       if (guess == 0) {
         decision.textContent = "Draw";
